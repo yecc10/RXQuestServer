53f6b3ddf80a91454b0b3c00d8794d21b1da1465
Administrator <Administrator@DESKTOP-5HK1A1I>
2020-06-12 15:21:12 +0800

    AA

--- RXQuestServer/Main.Designer.cs
+++ RXQuestServer/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RXQuestServer
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 336);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- RXQuestServer/Main.cs
+++ RXQuestServer/Main.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RXQuestServer
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
         }
