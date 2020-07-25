5245a7d407ac5c74a63b785c763b5f266bc76fce
Administrator <Administrator@DESKTOP-5HK1A1I>
2020-07-25 09:50:47 +0800

    add setup

---
+++ ProgrameSetup/SetUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Setup;
+using Microsoft.VisualStudio.Setup.Configuration;
+
+namespace ProgrameSetup
+{
+    public partial class SetUp : Form
+    {
+        public SetUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
