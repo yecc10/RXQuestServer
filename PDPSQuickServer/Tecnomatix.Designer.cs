e5010a61e3f5c64d21daa373d5f06903f4908c22
Administrator <Administrator@DESKTOP-5HK1A1I>
2020-07-31 10:28:02 +0800

    AAA

--- PDPSQuickServer/Tecnomatix.Designer.cs
+++ PDPSQuickServer/Tecnomatix.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PDPSQuickServer
 {
-    partial class Form1
+    partial class Tecnomatix
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Tecnomatix));
+            this.SuspendLayout();
+            // 
+            // Tecnomatix
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(899, 519);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Tecnomatix";
+            this.Text = "Tecnomatix";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ Tecnomatix/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tecnomatix
+{
+    public class Class1
+    {
+    }
+}
