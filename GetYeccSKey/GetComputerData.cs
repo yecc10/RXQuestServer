040ca8d69b1e85d4bde3f5a55eaa328e052cddf9
Administrator <Administrator@DESKTOP-5HK1A1I>
2020-09-02 15:20:01 +0800

    AAA

---
+++ GetYeccSKey/GetComputerData.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using System.Management;
+using System.Security.Policy;
+using Microsoft.Win32;
+using System.Windows.Forms;
+
+namespace GetYeccSKey
+{
+    class GetUserKeyData
+    {
+        private string GetCPUID()
+        {
+            ManagementClass managClass = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection moc = managClass.GetInstances();
+            string CPUID = null;
+            foreach (ManagementObject item in moc)
+            {
+                CPUID = item.Properties["processorid"].Value.ToString();
+                break;
+            }
+            return CPUID;
+        }
+        private string GetBoardID()
+        {
+            ManagementClass managClass = new ManagementClass("Win32_BaseBoard");
+            ManagementObjectCollection moc = managClass.GetInstances();
+            string BoardID = null;
+            foreach (ManagementObject item in moc)
+            {
+                BoardID = item.Properties["SerialNumber"].Value.ToString();
+                break;
+            }
+            return BoardID;
+        }
+        private string GetBIOSID()
+        {
+            ManagementClass managClass = new ManagementClass("Win32_BIOS");
+            ManagementObjectCollection moc = managClass.GetInstances();
+            string BIOSID = null;
+            foreach (ManagementObject item in moc)
+            {
+                BIOSID = item.Properties["SerialNumber"].Value.ToString();
+                break;
+            }
+            return BIOSID;
+        }
+        private string GetComputerID()
+        {
+            string ComputerId = GetBoardID() + GetBoardID() + GetCPUID();
+            return ComputerId;
+        }
+        public string GetHashProtectComputerID()
+        {
+            string ComputerId = GetComputerID()+ "yeccdesignforruixiang2020";
+           // int HashComputerId=ComputerId.GetHashCode();
+            return ComputerId;
+        }
+        public bool CheckUsrKey(string UserKey)
+        {
+            DateTime dateTime = DateTime.Now;
+            var Psh = new PasswordHasher();
+            //检查注册信息,如果未注册设定30天试用
+            PasswordHasher passwordHasher = new PasswordHasher();
+            GetComputerData getComputerData = new GetComputerData();
+            string protectid = getComputerData.GetHashProtectComputerID();
+            var Str = Psh.VerifyHashedPassword(UserKey, protectid);  // 使用PASSWORDHASH 数值进行对比
+            if (Convert.ToBoolean(Str))
+            {
+                RegOprate.WriteRegdit("Regkey", UserKey);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #region 注册操作集
+        class RegOprate
+        {
+            /// <summary>
+            /// 注册表存在检查
+            /// </summary>
+            /// <param name="name">待确认的注册表名称</param>
+            /// <returns></returns>
+            static public bool IsRegeditExit(string name)
+            {
+                bool _exit = false;
+                string[] subkeyName;
+                RegistryKey hkml = Registry.LocalMachine;
+                RegistryKey software = hkml.OpenSubKey("SOFTWARE", true);
+                RegistryKey admindir = software.OpenSubKey("RXYFYECHAOCHENG", true);
+                if (admindir == null)
+                {
+                    software.CreateSubKey("RXYFYECHAOCHENG");
+                }
+                admindir = software.OpenSubKey("RXYFYECHAOCHENG", true);
+                subkeyName = admindir.GetValueNames();
+                foreach (var KeyName in subkeyName)
+                {
+                    if (KeyName == name)
+                    {
+                        _exit = true;
+                        hkml.Close();
+                        software.Close();
+                        admindir.Close();
+                        return _exit;
+                    }
+                }
+                hkml.Close();
+                software.Close();
+                admindir.Close();
+                return _exit;
+            }
+            /// <summary>
+            /// 写入注册表
+            /// </summary>
+            /// <param name="name">名称</param>
+            /// <param name="value">值</param>
+            static public void WriteRegdit(string name, string value)
+            {
+                RegistryKey hklm = Registry.LocalMachine;
+                RegistryKey software = hklm.OpenSubKey("SOFTWARE", true);
+                RegistryKey admindir = software.OpenSubKey("RXYFYECHAOCHENG", true);
+                admindir.SetValue(name, value);
+                hklm.Close();
+                software.Close();
+                admindir.Close();
+            }
+            static public String GetRegValue(string str)
+            {
+                RegistryKey hklm = Registry.LocalMachine;
+                RegistryKey software = hklm.OpenSubKey("SOFTWARE", true);
+                RegistryKey admindir = software.OpenSubKey("RXYFYECHAOCHENG", true);
+                object str1 = admindir.GetValue(str);
+                hklm.Close();
+                software.Close();
+                admindir.Close();
+                return Convert.ToString(str1);
+            }
+
+        }
+        #endregion
+    }
+}
---
+++ GetYeccSKey/GetYeccSKey.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GetYeccSKey
+{
+    partial class GetYeccSKey
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GetYeccSKey));
+            this.UserApplicatCode = new System.Windows.Forms.TextBox();
+            this.KeyCode = new System.Windows.Forms.TextBox();
+            this.regValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.GetUserKey = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserApplicatCode
+            // 
+            this.UserApplicatCode.Location = new System.Drawing.Point(16, 45);
+            this.UserApplicatCode.Multiline = true;
+            this.UserApplicatCode.Name = "UserApplicatCode";
+            this.UserApplicatCode.Size = new System.Drawing.Size(645, 104);
+            this.UserApplicatCode.TabIndex = 0;
+            // 
+            // KeyCode
+            // 
+            this.KeyCode.Location = new System.Drawing.Point(16, 208);
+            this.KeyCode.Multiline = true;
+            this.KeyCode.Name = "KeyCode";
+            this.KeyCode.Size = new System.Drawing.Size(645, 121);
+            this.KeyCode.TabIndex = 0;
+            // 
+            // regValue
+            // 
+            this.regValue.AutoSize = true;
+            this.regValue.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.regValue.Location = new System.Drawing.Point(12, 9);
+            this.regValue.Name = "regValue";
+            this.regValue.Size = new System.Drawing.Size(142, 24);
+            this.regValue.TabIndex = 2;
+            this.regValue.Text = "用户申请码:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "注册码:";
+            // 
+            // GetUserKey
+            // 
+            this.GetUserKey.Location = new System.Drawing.Point(463, 346);
+            this.GetUserKey.Name = "GetUserKey";
+            this.GetUserKey.Size = new System.Drawing.Size(198, 56);
+            this.GetUserKey.TabIndex = 3;
+            this.GetUserKey.Text = "执行转换";
+            this.GetUserKey.UseVisualStyleBackColor = true;
+            this.GetUserKey.Click += new System.EventHandler(this.GetUserKey_Click);
+            // 
+            // GetYeccSKey
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 417);
+            this.Controls.Add(this.GetUserKey);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.regValue);
+            this.Controls.Add(this.KeyCode);
+            this.Controls.Add(this.UserApplicatCode);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GetYeccSKey";
+            this.Text = "GetYeccSKey";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserApplicatCode;
+        private System.Windows.Forms.TextBox KeyCode;
+        private System.Windows.Forms.Label regValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button GetUserKey;
+    }
+}
+
